467b834c8b44ce191b0f9b64b473ec1cdfb614d5
Alex Valladares <alex.valla101@gmail.com>
2021-01-17 17:20:34 -0500

    added all relevant files as well as Procfile & requirements.txt

---
+++ app.py
@@ -0,0 +1,196 @@
+import pandas as pd
+
+import numpy as np
+
+from flask import Flask, jsonify, render_template
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import text  #ES import text to use SQL text directly
+from sqlalchemy import create_engine, func, inspect
+
+# Connecting to the Postgres Database
+engine = create_engine(f'postgresql://postgres:50mL*android@localhost:5432/PL_Stats')
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Base Class
+stats_pl_2019 = Base.classes.pl_2019
+
+combined_fpl_2020 = Base.classes.combined_fpl_table
+
+
+# Creating Flask Application
+app = Flask(__name__)
+
+# Route 1
+@app.route("/")
+def home_route_function():
+    inspector = inspect(engine)
+
+    stats_list = inspector.get_columns('combined_fpl_table2')
+
+    clean_stats_list = ["Player",
+    "Nationality",
+    "Team",
+    "Player Position",
+    "Age",
+    "Games Played",
+    "Games Started",
+    "Total FPL Points",
+    "Minutes Played",
+    "Selected By Percent",
+    "Current FPL Price",
+    "Transfers In (This Gameweek)",
+    "FPL Points/FPL Value",
+    "Goals Scored",
+    "Assists",
+    "Penalty Kicks Scored",
+    "Penalty Kicks Attempted",
+    "Yellow Cards",
+    "Red Cards",
+    "Goals Per 90",
+    "Assists Per 90",
+    "Goals + Assists Per 90",
+    "Goals - Penalty Kicks Per 90",
+    "Goals + Assists - Penalty Kicks Per 90",
+    "Expected Goals",
+    "Non Penalty Expected Goals",
+    "Expected Assists",
+    "Expected Goals Per 90",
+    "Expected Assists Per 90",
+    "Expected Goals + Expected Assists Per 90",
+    "Non Penalty Expected Goals Per 90",
+    "Non Penalty Expected Goals + Expected Assists Per 90",
+    "Season"]
+
+    zipped_columns = zip(stats_list, clean_stats_list)
+    
+    # Working Query Using SQLAlchemy Classes
+    session = Session(engine)
+
+    query_player = session.query(combined_fpl_2020.player).order_by(combined_fpl_2020.total_points.desc()).all()
+
+    connection = engine.connect()
+    query_fpl_positions = connection.execute("select distinct player_position from combined_fpl_table2")
+
+    # session.query(combined_fpl_2020.player_positon).order_by(combined_fpl_2020.total_points.desc()).all()
+    return render_template('index.html', query_player = query_player, zipped_columns = zipped_columns, query_fpl_positions = query_fpl_positions)
+
+# TESTING ROUTES FROM HERE TO BOTTOM
+
+# Testing Route
+
+@app.route("/query_all_players/<player>")
+def player_route(player):
+    connection = engine.connect()
+
+    session = Session(engine)
+
+    query_fpl_view = connection.execute(f"select * from combined_fpl_table2 where combined_fpl_table2.player='{player}'", player=player)
+
+    session.close()
+
+    return jsonify([dict(row) for row in query_fpl_view])
+
+
+    # for query in query_fpl_view: 
+    #     print(query)
+
+    # all_rows = []
+    # for player, nation, team, age, games_played, starts, total_points, minutes, selected_by_percent, current_price,transfers_in, value_season, Goals, Assists, Penalty_Kicks_Scored, Penalty_Kicks_Attempted, Yellow_Cards, Red_Cards, Goals_Per_90, Assists_Per_90, Goals_Plus_Assists_Per_90, Goals_Minus_Penalty_Kicks_Per_90, Goals_Plus_Assists_Minus_Penalty_Kicks_Per_90, Expected_Goals, Non_Penalty_Expected_Goals, Expected_Assists, Expected_Goals_Per_90, Expected_Assists_Per_90, Expected_Goals_Plus_Expected_Assists_Per_90, Non_Penalty_Expected_Goals_Per_90, Non_Penalty_Expected_Goals_Plus_Expected_Assists_Per_90, Season in query_fpl_view:
+    # # for player in query_player:
+    #     test_dict = {}
+    #     test_dict["player"] = player
+    #     test_dict["nation"] = nation
+    #     test_dict["team"] = team
+    #     test_dict["age"] = age
+    #     test_dict["games_played"] = games_played
+    #     test_dict["starts"] = starts
+    #     test_dict["total_points"] = total_points
+    #     test_dict["minutes"] = minutes
+    #     test_dict["selected_by_percent"] = selected_by_percent
+    #     test_dict["current_price"] = current_price
+    #     test_dict["transfers_in"] = transfers_in
+    #     test_dict["value_season"] = value_season
+    #     test_dict["Goals"] = Goals
+    #     test_dict["Assists"] = Assists
+    #     test_dict["Penalty_Kicks_Scored"] = Penalty_Kicks_Scored
+    #     test_dict["Penalty_Kicks_Attempted"] = Penalty_Kicks_Attempted
+    #     test_dict["Yellow_Cards"] = Yellow_Cards
+    #     test_dict["Red_Cards"] = Red_Cards
+    #     test_dict["Goals_Per_90"] = Goals_Per_90
+    #     test_dict["Assists_Per_90"] = Assists_Per_90
+    #     test_dict["Goals_Plus_Assists_Per_90"] = Goals_Plus_Assists_Per_90
+    #     test_dict["Goals_Minus_Penalty_Kicks_Per_90"] = Goals_Minus_Penalty_Kicks_Per_90
+    #     test_dict["Goals_Plus_Assists_Minus_Penalty_Kicks_Per_90"] = Goals_Plus_Assists_Minus_Penalty_Kicks_Per_90
+    #     test_dict["Expected_Goals"] = Expected_Goals
+    #     test_dict["Non_Penalty_Expected_Goals"] = Non_Penalty_Expected_Goals
+    #     test_dict["Expected_Assists"] = Expected_Assists
+    #     test_dict["Expected_Goals_Per_90"] = Expected_Goals_Per_90
+    #     test_dict["Expected_Assists_Per_90"] = Expected_Assists_Per_90
+    #     test_dict["Expected_Goals_Plus_Expected_Assists_Per_90"] = Expected_Goals_Plus_Expected_Assists_Per_90
+    #     test_dict["Non_Penalty_Expected_Goals_Per_90"] = Non_Penalty_Expected_Goals_Per_90
+    #     test_dict["Non_Penalty_Expected_Goals_Plus_Expected_Assists_Per_90"] = Non_Penalty_Expected_Goals_Plus_Expected_Assists_Per_90
+    #     test_dict["Season"] = Season
+        
+
+    #     all_rows.append(test_dict)
+    # unique_player = list(np.ravel(query_fpl_view))
+
+    # return jsonify(all_rows) 
+
+# Querying player & requested stat in order by descending. Used for interactive bar graph
+@app.route("/<stat>")
+def stat_testing(stat):
+    connection = engine.connect()
+
+    result = connection.execute(f"select player, {stat} from combined_fpl_table order by {stat} desc")
+    # for record in result:
+    #     print(record)
+    return jsonify([dict(row) for row in result])
+
+# Querying all keys based on price filter
+@app.route("/query_by_value/<price>")
+def filter_by_price(price):
+    connection = engine.connect()
+
+    query_player_price_limit = connection.execute(f"select * from combined_fpl_table2 where current_price <='{price}' order by current_price desc, total_points desc", price = price)
+
+    return jsonify([dict(row) for row in query_player_price_limit])
+
+# ROUTE TO QUERY 2 PLAYERS SIMULTANEOUSLY
+@app.route("/query_all_players/<player1>/<player2>")
+def compare_players_route(player1, player2):
+    connection = engine.connect()
+
+    session = Session(engine)
+
+    # query_fpl_view1 = connection.execute(f"select * from combined_fpl_table2 where combined_fpl_table2.player='{player1}'", player=player1)
+
+    # query_fpl_view2 = connection.execute(f"select * from combined_fpl_table2 where combined_fpl_table2.player='{player2}'", player=player2)
+
+    query_fpl_view1 = connection.execute(f"select * from combined_fpl_table_test_gk2 where combined_fpl_table_test_gk2.player='{player1}'", player=player1)
+
+    query_fpl_view2 = connection.execute(f"select * from combined_fpl_table_test_gk2 where combined_fpl_table_test_gk2.player='{player2}'", player=player2)
+
+    session.close()
+
+    return jsonify([dict(row) for row in query_fpl_view1], [dict(row) for row in query_fpl_view2])
+
+# ROUTE TO QUERY PLAYERS BY POSITION
+@app.route("/query_by_position/<position>")
+def filter_by_position(position):
+    connection = engine.connect()
+
+    query_player_position = connection.execute(f"select * from combined_fpl_table2 where player_position = '{position}' order by total_points desc", position = position)
+
+    return jsonify([dict(row) for row in query_player_position])
+
+# Define main behavior
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/plot_test.js
@@ -0,0 +1,468 @@
+// Dark Mode Button
+
+var darkMode = document.getElementById("dark_mode");
+
+darkMode.addEventListener("click", function(){
+  var body = document.getElementById("body");
+  body.style.backgroundColor = "black";
+
+  var headerPicture = document.getElementById("header_pic");
+  headerPicture.style.backgroundColor="#eae9de85";
+
+  var index;
+  var headerText = document.getElementsByClassName("header_for_graph");
+  for (index = 0; index < headerText.length; ++index) {
+    headerText[index].style.color ="#eae9de85";
+}
+  // If Reset Dark Mode Doesn't Exist, it will be created.
+  // Else, no new buttons will be created
+  if($('#reset_dark_mode').length == 0){
+    var resetDarkMode = document.createElement("button");
+  }
+  else{console.log("dark mode already clicked");
+
+  };
+
+  resetDarkMode.className = "btn btn-primary";
+  resetDarkMode.textContent = "Reset Color Scheme";
+  resetDarkMode.setAttribute("id", "reset_dark_mode");
+  
+  var headerElement = document.getElementById("header");
+
+  headerElement.insertAdjacentElement('beforeend', resetDarkMode);
+
+  resetDarkMode.addEventListener("click", function(){
+    console.log("reset color");
+    var body = document.getElementById("body");
+    body.style.backgroundColor = "#4bb370";
+
+    var headerPicture = document.getElementById("header_pic");
+    headerPicture.style.backgroundColor="#4bb370";
+
+    var index;
+    var headerText = document.getElementsByClassName("header_for_graph");
+    for (index = 0; index < headerText.length; ++index) {
+      headerText[index].style.color ="black";
+    }
+  });
+
+});
+
+
+// FUNCTIONAL bar graph that will create a bar graph, filtering stats in descending order
+// Produces the Bar Graph based on Stat Selected
+function interactivePlot(stat){
+var statsDropdown = d3.select(statsList);
+var stat = statsDropdown.property("value");
+var url2 = `${stat}`
+d3.json(url2).then(function(data) {
+
+    console.log(data);
+
+    var PlayerName = data.map(Player => Player["player"]);
+    var statQueried = data.map(Player => Player[stat]);
+    
+
+    var trace1 = {
+        x: PlayerName,
+        y: statQueried,
+        type: "bar"
+      };
+      
+      var data = [trace1];
+      
+      var layout = {
+        title: "2020/2021 Premier League Top Performers",
+        margin: {b:185},
+        yaxis: {automargin: true},
+        xaxis: {type: 'category'}
+      };
+      
+      Plotly.newPlot("statBarGraph", data, layout);
+  });
+};
+
+
+stats = d3.select("#statsList")
+
+stats.on("change", interactivePlot)
+
+
+// Function to query sql database based on user input for player value
+// Successfully returns all the records that are =< value inserted 
+
+function filterByPrice(){
+
+  var button = document.getElementById("priceFilterButton");
+
+  button.addEventListener("click", function formValidate() {
+
+    var formValue = document.getElementById("priceFilter").value;
+  
+    // console.log(formValue);  
+
+    var test_url = `/query_by_value/${formValue}`;
+
+      d3.json(test_url).then(function(data){
+      console.table(data);
+      var table = new Tabulator("#playerDataTable", {
+      data:data, //assign data to table
+      // autoColumns:true, //create columns from data field names
+      height: "500px",
+      layout: "fitDataStretch",
+      columns:[
+        {title: "Player", field: "player", frozen: true},
+        {title: "Team", field: "team"},
+        {title: "Nationality", field: "nation"},
+        {title: "Position", field: "player_position"},
+        {title: "Age", field: "age"},
+        {title: "FPL Value", field: "current_price"},
+        {title: "FPL Points", field: "total_points"},
+        {title: "FPL Points/FPL Value", field: "value_season"},
+        {title: "Goals", field: "goals"},
+        {title: "Assists", field: "assists"},
+        {title: "Selected By (%)", field: "selected_by_percent"},
+        {title: "Transferred In (This Week)", field: "transfers_in"},
+        {title: "Minutes Played", field: "minutes"},
+        {title: "Games Played", field: "games_played"},
+        {title: "Games Started", field: "starts"},
+        {title: "PKs Scored", field: "penalty_kicks_scored"},
+        {title: "PKs Attempted", field: "penalty_kicks_attempted"},
+        {title: "Expected Goals", field: "expected_goals"},
+        {title: "Non Penalty xG", field: "non_penalty_expected_goals"},
+        {title: "Expected Assists", field: "expected_assists"},
+        {title: "Expected Goals per 90", field: "expected_goals_per_90"},
+        {title: "Expected Assists per 90", field: "expected_assists_per_90"},
+        {title: "xG + xA per 90", field: "expected_goals_plus_expected_assists_per_90"},
+        {title: "Non Penalty xG + xA per 90", field: "non_penalty_expected_goals_plus_expected_assists_per_90"},
+        {title: "Non Penalty xG per 90", field: "non_penalty_expected_goals_per_90"},
+        {title: "Goals Per 90", field: "goals_per_90"},
+        {title: "Goals - PK Per 90", field: "goals_minus_penalty_kicks_per_90"},
+        {title: "Assists Per 90", field: "assists_per_90"},
+        {title: "G + A Per 90", field: "goals_plus_assists_per_90"},
+        {title: "G + A  - PK Per 90", field: "goals_plus_assists_minus_penalty_kicks_per_90"},
+        {title: "Yellow Cards", field: "yellow_cards"},
+        {title: "Red Cards", field: "red_cards"},         
+      ]
+  });
+    });
+  });
+
+};
+var priceButton = document.getElementById("priceFilterButton")
+priceButton.addEventListener("change", filterByPrice())
+// Function to Filter Table by Position
+
+var positionFilterButton = document.getElementById("positionButton");
+
+positionFilterButton.addEventListener("click", function(){
+  // console.log("it worked");
+
+  var position = d3.select(queryPosition);
+
+  var positionValue = position.property("value");
+
+  var url2 = `/query_by_position/${positionValue}`
+  d3.json(url2).then(function(data){
+    console.log(data)
+    var table = new Tabulator("#playerDataTable", {
+      data:data, //assign data to table
+      // autoColumns:true, //create columns from data field names
+      height: "500px",
+      layout: "fitDataStretch",
+      columns:[
+        {title: "Player", field: "player", frozen: true},
+        {title: "Team", field: "team"},
+        {title: "Nationality", field: "nation"},
+        {title: "Position", field: "player_position"},
+        {title: "Age", field: "age"},
+        {title: "FPL Value", field: "current_price"},
+        {title: "FPL Points", field: "total_points"},
+        {title: "FPL Points/FPL Value", field: "value_season"},
+        {title: "Goals", field: "goals"},
+        {title: "Assists", field: "assists"},
+        {title: "Selected By (%)", field: "selected_by_percent"},
+        {title: "Transferred In (This Week)", field: "transfers_in"},
+        {title: "Minutes Played", field: "minutes"},
+        {title: "Games Played", field: "games_played"},
+        {title: "Games Started", field: "starts"},
+        {title: "PKs Scored", field: "penalty_kicks_scored"},
+        {title: "PKs Attempted", field: "penalty_kicks_attempted"},
+        {title: "Expected Goals", field: "expected_goals"},
+        {title: "Non Penalty xG", field: "non_penalty_expected_goals"},
+        {title: "Expected Assists", field: "expected_assists"},
+        {title: "Expected Goals per 90", field: "expected_goals_per_90"},
+        {title: "Expected Assists per 90", field: "expected_assists_per_90"},
+        {title: "xG + xA per 90", field: "expected_goals_plus_expected_assists_per_90"},
+        {title: "Non Penalty xG + xA per 90", field: "non_penalty_expected_goals_plus_expected_assists_per_90"},
+        {title: "Non Penalty xG per 90", field: "non_penalty_expected_goals_per_90"},
+        {title: "Goals Per 90", field: "goals_per_90"},
+        {title: "Goals - PK Per 90", field: "goals_minus_penalty_kicks_per_90"},
+        {title: "Assists Per 90", field: "assists_per_90"},
+        {title: "G + A Per 90", field: "goals_plus_assists_per_90"},
+        {title: "G + A  - PK Per 90", field: "goals_plus_assists_minus_penalty_kicks_per_90"},
+        {title: "Yellow Cards", field: "yellow_cards"},
+        {title: "Red Cards", field: "red_cards"},         
+      ]
+  });
+  })
+}
+);
+
+              // NEW CHART
+// Route for Player Offensive Stats. Generates a Bar Graph
+
+function filterByPlayer(){
+  // var buttonPlayer = document.getElementById("playerName");
+
+  var getPlayerStatsButton = document.getElementById("getPlayerStatsButton");
+  getPlayerStatsButton.addEventListener("click", function playerSelected() {
+
+    // var playerDropdown = d3.select("#playerName");
+    var player = document.getElementById("playerName").value;
+    var test_url = `/query_all_players/${player}`;
+    d3.json(test_url).then(function(data){
+      console.log(data);
+
+      var PlayerName = data.map(Player => Player["player"]);
+      var totalPoints = data.map(Player => Player["total_points"]);
+      var totalGoals = data.map(Player => Player["goals"]);
+      var totalAssists = data.map(Player => Player["assists"]);
+      var xGoals = data.map(Player => Player["expected_goals"]);
+      var xAssists = data.map(Player => Player["expected_assists"]);
+
+      var trace1 = {
+        x: PlayerName,
+        y: totalPoints,
+        name: "Total Points",
+        type: "bar",
+        // text: totalPoints
+      };
+
+      var trace2 = {
+        x: PlayerName,
+        y: totalGoals,
+        name: "Total Goals Scored",
+        type: "bar",
+        // text: totalGoals
+      };
+
+      var trace3 = {
+        x: PlayerName,
+        y: xGoals,
+        name: "Expected Goals",
+        type: "bar",
+        // text: xGoals
+      };
+
+      var trace4 = {
+        x: PlayerName,
+        y: totalAssists,
+        name: "Total Assists",
+        type: "bar",
+        // text: totalAssists
+      };
+
+      var trace5 = {
+        x: PlayerName,
+        y: xAssists,
+        name: "Expected Assists",
+        type: "bar",
+        // text: xAssists
+      };
+
+      var data = [trace1, trace2, trace3, trace4, trace5];
+      
+      var layout = {
+        title: `2020/2021 Premier League Stats: ${PlayerName}`,
+        yaxis: {automargin: true},
+        xaxis: {type: 'category'},
+        // barmode: 'group'
+      };
+      
+      Plotly.newPlot("playerBoxPlot", data, layout);
+  
+  });
+});
+};
+
+var getPlayerStatsButton = document.getElementById("getPlayerStatsButton");
+
+getPlayerStatsButton.addEventListener("change", filterByPlayer())
+
+                // NEW GRAPH TO COMPARE PLAYERS
+
+// Graphing Function to compare offensive stats of 2 players
+function comparePlayers(){
+  // var buttonPlayer2 = document.getElementById("comparePlayer2");
+
+  // buttonPlayer2.addEventListener("click", function playersSelected() {
+  var buttonPlayer2 = document.getElementById("comparePlayersButton");
+
+  buttonPlayer2.addEventListener("click", function playersSelected() {
+
+    // var playerDropdown = d3.select("#playerName");
+    var player1 = document.getElementById("comparePlayer1").value;
+    var player2 = document.getElementById("comparePlayer2").value;
+    var test_url = `/query_all_players/${player1}/${player2}`;
+    d3.json(test_url).then(function(data){
+      // console.log(data[0]);
+      // console.log(data[1]);
+
+      // Compare Player 1
+      var PlayerName1 = data[0].map(Player => Player["player"]);
+      var totalPoints1 = data[0].map(Player => Player["total_points"]);
+      var totalGoals1 = data[0].map(Player => Player["goals"]);
+      var totalAssists1 = data[0].map(Player => Player["assists"]);
+      var totalG_plus_A_per90_1 = data[0].map(Player => Player["goals_plus_assists_per_90"]);
+      var xGoals1 = data[0].map(Player => Player["expected_goals"]);
+      var xAssists1 = data[0].map(Player => Player["expected_assists"]);
+      var playerPosition1 = data[0].map(Player => Player["player_position"]);
+      // Player 1 GK Stats
+      var cleanSheet1 = data[0].map(Player => Player["clean_sheets"]);
+      var cleanSheetPercentage1 = data[0].map(Player => Player["clean_sheet_percentage"]);
+      var goalsConceded1 = data[0].map(Player => Player["goals_against"]);
+      var goalsConcededPerMatch1 = data[0].map(Player => Player["goals_against_per_90"]);
+      var pkAttemptAgainst1 = data[0].map(Player => Player["pk_attempts_against"]);
+      var pkSaved1 = data[0].map(Player => Player["penalty_kicks_saved"]);
+      var pkScoredOn1 = data[0].map(Player => Player["penalty_kicks_scored_on"]);
+      var pkMissedAgainst1 = data[0].map(Player => Player["penalty_kicks_missed"]);
+      var gkSaves1 = data[0].map(Player => Player["saves"]);
+      var gkSavePercentage1 = data[0].map(Player => Player["save_percentage"]);
+      var gkShotsOnTargetAgainst1 = data[0].map(Player => Player["shots_on_target_against"]);
+    
+
+
+      // Compare Player 2
+      var PlayerName2 = data[1].map(Player => Player["player"]);
+      var totalPoints2 = data[1].map(Player => Player["total_points"]);
+      var totalGoals2 = data[1].map(Player => Player["goals"]);
+      var totalAssists2 = data[1].map(Player => Player["assists"]);
+      var totalG_plus_A_per90_2 = data[1].map(Player => Player["goals_plus_assists_per_90"]);
+      var xGoals2 = data[1].map(Player => Player["expected_goals"]);
+      var xAssists2 = data[1].map(Player => Player["expected_assists"]);
+      var playerPosition2 = data[1].map(Player => Player["player_position"]);
+      // Player 2 GK Stats
+      var cleanSheet2 = data[1].map(Player => Player["clean_sheets"]);
+      var cleanSheetPercentage2 = data[0].map(Player => Player["clean_sheet_percentage"]);
+      var goalsConceded2 = data[1].map(Player => Player["goals_against"]);
+      var goalsConcededPerMatch2 = data[1].map(Player => Player["goals_against_per_90"]);
+      var pkAttemptAgainst2 = data[1].map(Player => Player["pk_attempts_against"]);
+      var pkSaved2 = data[1].map(Player => Player["penalty_kicks_saved"]);
+      var pkScoredOn2 = data[1].map(Player => Player["penalty_kicks_scored_on"]);
+      var pkMissedAgainst2 = data[1].map(Player => Player["penalty_kicks_missed"]);
+      var gkSaves2 = data[1].map(Player => Player["saves"]);
+      var gkSavePercentage2 = data[1].map(Player => Player["save_percentage"]);
+      var gkShotsOnTargetAgainst2 = data[1].map(Player => Player["shots_on_target_against"]);
+
+      if(playerPosition1 != "Goalkeeper"){
+        var trace1 = {
+          x: ["Points", "Goals", "Assists", "Goals + Assists per 90", "xGoals", "xAssists"],
+          y: [parseInt(totalPoints1), parseInt(totalGoals1), parseInt(totalAssists1), parseFloat(totalG_plus_A_per90_1), parseFloat(xGoals1), parseFloat(xAssists1) ],
+          name: `${PlayerName1}`,
+          type: "bar",
+        };
+      }
+      else{
+        var trace1 = {
+          x: ["Points", "Saves", "Save Percentage", "Shots On Target Against","Clean Sheets", "Clean Sheet Percentage", "Goals Conceded", "Goals Per Match", "PKs Attempted Against", "PKs Saved", "PKs Scored Against", "PKs Missed Against"],
+          y: [parseInt(totalPoints1), parseInt(gkSaves1), parseInt(gkSavePercentage1), parseInt(gkShotsOnTargetAgainst1), parseInt(cleanSheet1), parseFloat(cleanSheetPercentage1), parseInt(goalsConceded1), parseFloat(goalsConcededPerMatch1), parseInt(pkAttemptAgainst1), parseInt(pkSaved1), parseInt(pkScoredOn1), parseInt(pkMissedAgainst1)],
+          name: `${PlayerName1}`,
+          type: "bar",
+        };
+      }
+
+      if(playerPosition2 != "Goalkeeper"){
+        var trace2 = {
+          x: ["Points", "Goals", "Assists", "Goals + Assists per 90", "xGoals", "xAssists"],
+          y: [parseInt(totalPoints2), parseInt(totalGoals2), parseInt(totalAssists2), parseFloat(totalG_plus_A_per90_2), parseFloat(xGoals2), parseFloat(xAssists2) ],
+          name: `${PlayerName2}`,
+          type: "bar",
+        };  
+      }
+      else{
+        var trace2 = {
+          x: ["Points", "Saves", "Save Percentage", "Shots On Target Against","Clean Sheets", "Clean Sheet Percentage", "Goals Conceded", "Goals Per Match", "PKs Attempted Against", "PKs Saved", "PKs Scored Against", "PKs Missed Against"],
+          y: [parseInt(totalPoints2), parseInt(gkSaves2), parseInt(gkSavePercentage2), parseInt(gkShotsOnTargetAgainst2), parseInt(cleanSheet2), parseFloat(cleanSheetPercentage2), parseInt(goalsConceded2), parseFloat(goalsConcededPerMatch2), parseInt(pkAttemptAgainst2), parseInt(pkSaved2), parseInt(pkScoredOn2), parseInt(pkMissedAgainst2)],
+          name: `${PlayerName2}`,
+          type: "bar",
+        };
+      }
+
+      // var trace1 = {
+      //   x: ["Points", "Goals", "Assists", "Goals + Assists per 90", "xGoals", "xAssists"],
+      //   y: [parseInt(totalPoints1), parseInt(totalGoals1), parseInt(totalAssists1), parseFloat(totalG_plus_A_per90_1), parseFloat(xGoals1), parseFloat(xAssists1) ],
+      //   name: `${PlayerName1}`,
+      //   type: "bar",
+      // };
+
+      // var trace2 = {
+      //   x: ["Points", "Goals", "Assists", "Goals + Assists per 90", "xGoals", "xAssists"],
+      //   y: [parseInt(totalPoints2), parseInt(totalGoals2), parseInt(totalAssists2), parseFloat(totalG_plus_A_per90_2), parseFloat(xGoals2), parseFloat(xAssists2) ],
+      //   name: `${PlayerName2}`,
+      //   type: "bar",
+      // };
+
+      var data = [trace1, trace2];
+      
+      var layout = {
+        title: `2020/2021 Premier League Stats: ${PlayerName1} vs ${PlayerName2}`,
+        yaxis: {automargin: true},
+        xaxis: {type: 'category'},
+        barmode: 'group',
+        margin: {b:100},
+      };
+      
+      Plotly.newPlot("comparePlayerGraph", data, layout);
+
+      
+  
+  });
+});
+};
+
+var playerComparedFinalButton = document.getElementById("comparePlayersButton");
+playerComparedFinalButton.addEventListener("change", comparePlayers());
+
+// playerCompared2.addEventListener("change", comparePlayers())
+
+// FROM HERE TO END IS TESTING SECTION. DO NOT ERASE ANYTHING ABOVE
+
+//                        !!! DO NOT ERASE !!!
+
+//              THIS WORKS
+// var button = document.getElementById("priceFilterButton");
+
+// button.addEventListener("click", function(data) {
+
+//   (console.log("it worked"));
+
+// });
+
+
+
+//            TESTING CHART JS CODE RADAR CHART
+
+
+// var trace1 = {
+//   labels: ["Goals", "Assists", "xGoals", "xAssists"],
+//   datasets: [{
+//     label: `${PlayerName1}`,
+//     data: [parseInt(totalGoals1), parseInt(totalAssists1), parseFloat(xGoals1), parseFloat(xAssists1)]
+//   }, {
+//     label: `${PlayerName2}`,
+//     data: [parseInt(totalGoals2), parseInt(totalAssists2), parseFloat(xGoals2), parseFloat(xAssists2)]
+//   }]
+// };
+
+// var data = trace1;
+
+// var ctx = document.getElementById('comparePlayerGraph');
+
+//       var myRadarChart = new Chart(ctx, {
+//         type: 'radar',
+//         data: data,
+//         // options: options
+//     });
+
+
