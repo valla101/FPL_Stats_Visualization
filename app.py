b34d03001cf6b347be48478bf2dd56748abd8e8f
Alex Valladares <alex.valla101@gmail.com>
2021-04-30 14:01:11 -0400

    cleaned up code

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text  #ES import text to use SQL text directly
 from sqlalchemy import create_engine, func, inspect
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+# FROM HERE DO NOT DELETE
 
 # Connecting to the Postgres Database
 engine = create_engine(f'postgres://wbqnbbqkykqdaj:63bf019edf08344171db6162cbfdd7dc8bb288a6068fdb6f029eeb18eb53271c@ec2-3-215-76-208.compute-1.amazonaws.com:5432/ddad815989gh9j')
